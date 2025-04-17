01f08b7806db11a47d12f1f56ca41cf4f5552f42
Nick Savage <nick@nicksavage.ca>
2025-04-17 16:28:48 -0400

    formatting

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -364,8 +364,7 @@ typedef uint64 *pagetable_t; // 512 PTEs
 
 #ifdef LAB_PGTBL
 #define SUPERPGSIZE (2 * (1 << 20)) // bytes per page
-#define SUPERPGROUNDUP(sz)  (((sz)+SUPERPGSIZE-1) & ~(SUPERPGSIZE-1))
-#endif
+#define SUPERPGROUNDUP(sz) (((sz) + SUPERPGSIZE - 1) & ~(SUPERPGSIZE - 1))
 
 #define PGROUNDUP(sz)  (((sz)+PGSIZE-1) & ~(PGSIZE-1))
 #define PGROUNDDOWN(a) (((a)) & ~(PGSIZE-1))
